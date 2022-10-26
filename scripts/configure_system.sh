848894318a97f010a6fc637a34fb770014f19abb
Stephen Bader <sjbader@users.noreply.github.com>
2022-10-26 14:37:24 -0500

    Updated configure_system script

--- scripts/configure_system.sh
+++ scripts/configure_system.sh
@@ -2,9 +2,9 @@
 
 # Create scripts directory if it doesn't exist
 [ ! -d "/home/ec2-user/scripts" ] && /usr/bin/mkdir /home/ec2-user/scripts
-/usr/bin/cd /home/ec2-user/scripts/
 
 # Download our scripts from GitHub
+/usr/bin/cd /home/ec2-user/scripts/
 /usr/bin/wget -q https://raw.githubusercontent.com/sjbader/terraform_deploy_k8s_ec2/master/scripts/generate_ansible_inventory_file.py
 /usr/bin/wget -q https://raw.githubusercontent.com/sjbader/terraform_deploy_k8s_ec2/master/scripts/generate_hosts_file.py
 
@@ -19,4 +19,18 @@
 /usr/bin/amazon-linux-extras enable ansible2
 /usr/bin/yum clean metadata
 /usr/bin/yum install ansible -y
+
+# Create ansible directory if it doesn't exist
+[ ! -d "/home/ec2-user/ansible" ] && /usr/bin/mkdir /home/ec2-user/ansible
+
+# Download Ansible playbooks
+/usr/bin/cd /home/ec2-user/ansible/
+/usr/bin/wget -q https://raw.githubusercontent.com/sjbader/terraform_deploy_k8s_ec2/master/ansible/update_ansible_config.yml
+/usr/bin/wget -q https://raw.githubusercontent.com/sjbader/terraform_deploy_k8s_ec2/master/ansible/copy_hosts_file.yml
+/usr/bin/wget -q https://raw.githubusercontent.com/sjbader/terraform_deploy_k8s_ec2/master/ansible/yum_update_all_hosts.yml
+
+# Run the Ansible playbooks
+/usr/bin/ansible-playbook -i /home/ec2-user/scripts/output/ansible_hosts.txt /home/ec2-user/ansible/update_ansible_config.yml
+/usr/bin/ansible-playbook --private-key /home/ec2-user/keys/ssh_key.pem -i /home/ec2-user/scripts/output/ansible_hosts.txt /home/ec2-user/ansible/copy_hosts_file.yml
+/usr/bin/ansible-playbook --private-key /home/ec2-user/keys/ssh_key.pem -i /home/ec2-user/scripts/output/ansible_hosts.txt /home/ec2-user/ansible/yum_update_all_hosts.yml
 
